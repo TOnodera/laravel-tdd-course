c493f9e63764a671a2088f6c1e3797a43ecec930
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-10-23 15:32:46 +0900

    テスト作成

--- laravel/tests/Feature/SignUpControllerTest.php
+++ laravel/tests/Feature/SignUpControllerTest.php
@@ -38,4 +38,25 @@ class SignUpControllerTest extends TestCase
         $this->assertNotNull($user);
         $this->assertTrue(Hash::check('abcd1234', $user->password));
     }
+
+    public function test不正なデータではユーザー登録出来ない()
+    {
+        $url = 'signup';
+        // $this->post($url, [])
+        //     ->assertRedirect();
+
+        $this->post($url, ['name'=>''])->assertSessionHasErrors(['name'=>'名前は必ず指定してください。']);
+        $this->post($url, ['name'=>str_repeat('a', 21)])->assertSessionHasErrors(['name' => '名前は、20文字以下で指定してください。']);
+        $this->post($url, ['name'=>str_repeat('a', 20)])->assertSessionDoesntHaveErrors('name');
+
+        $this->post($url, ['email' => ''])->assertSessionHasErrors(['email'=>'メールアドレスは必ず指定してください。']);
+        $this->post($url, ['email' => 'aa@bb@cc'])->assertSessionHasErrors(['email'=>'メールアドレスには、有効なメールアドレスを指定してください。']);
+        
+        User::factory()->create(['email' => 'aa@bb.net']);
+        $this->post($url, ['email' => 'aa@bb.net'])->assertSessionHasErrors(['email' => 'メールアドレスの値は既に存在しています。']);
+
+        $this->post($url, ['password'=>''])->assertSessionHasErrors(['password'=>'パスワードは必ず指定してください。']);
+        $this->post($url, ['password'=>'abcd123'])->assertSessionHasErrors(['password'=>'パスワードは、8文字以上で指定してください。']);
+        $this->post($url, ['password'=>'abcd1234'])->assertSessionDoesntHaveErrors(['password']);
+    }
 }
