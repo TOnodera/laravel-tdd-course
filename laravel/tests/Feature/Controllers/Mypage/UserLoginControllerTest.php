0a6e792c4277c1ac9b1d9e595a6333087f144385
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-10-23 19:34:27 +0900

    完了

--- laravel/tests/Feature/Controllers/Mypage/UserLoginControllerTest.php
+++ laravel/tests/Feature/Controllers/Mypage/UserLoginControllerTest.php
@@ -6,6 +6,7 @@ namespace Tests\Feature\Controllers\Mypage;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 /**
@@ -94,5 +95,47 @@ final class UserLoginControllerTest extends TestCase
         $this->from($url)->followingRedirects()->post($url, $postData)
             ->assertSee('メールアドレスかパスワードが間違っています。')
         ;
+    }
+
+    public function test認証エラーなのでValidationExceptionの例外が発生する()
+    {
+        $this->withoutExceptionHandling(); // 例外発生のテストでは必ず必要。ないとハンドリングされてしまう。
+
+        $postData = [
+            'email' => 'aaa@bbb.net',
+            'password' => 'abcd1234',
+        ];
+
+        // $this->expectException(ValidationException::class);
+
+        try {
+            $this->post('mypage/login', $postData);
+            static::fail('ValidationExceptionの例外が発生しませんでした。');
+        } catch (ValidationException $e) {
+            static::assertSame('メールアドレスかパスワードが間違っています。', $e->errors()['email'][0]);
+        }
+    }
+
+    public function test認証OKなのでvalidationExceptionの例外がでない()
+    {
+        $this->withoutExceptionHandling(); // 例外発生のテストでは必ず必要。ないとハンドリングされてしまう。
+        $postData = [
+            'email' => 'aaa@bbb.net',
+            'password' => 'abcd1234',
+        ];
+
+        $dbData = [
+            'email' => 'aaa@bbb.net',
+            'password' => bcrypt('abcd1234'),
+        ];
+
+        User::factory()->create($dbData);
+
+        try {
+            $this->post('mypage/login', $postData);
+            static::assertTrue(true);
+        } catch (ValidationException $e) {
+            static::fail('ValidationExceptionの例外が発生しました。');
+        }
     }
 }
