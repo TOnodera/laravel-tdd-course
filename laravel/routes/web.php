d865746d6861c2a649f295dd37a2f838d8036d80
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-10-23 20:51:42 +0900

    フォーマット

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -30,9 +30,9 @@ Route::get('signup/', [SignUpController::class, 'index']);
 Route::post('signup/', [SignUpController::class, 'store']);
 
 //ログインページ
-Route::get('mypage/login', [UserLoginController::class, 'index'])->name('login');// name('login')でデフォルトの認証で使用されている route('login')に対応
+Route::get('mypage/login', [UserLoginController::class, 'index'])->name('login'); // name('login')でデフォルトの認証で使用されている route('login')に対応
 Route::post('mypage/login', [UserLoginController::class, 'login']);
 
 Route::middleware('auth')->group(function () {
-    Route::get('mypage/blogs', [BlogMypageController::class,'index']);
+    Route::get('mypage/blogs', [BlogMypageController::class, 'index']);
 });
--- laravel/tests/Feature/Mypage/BlogMypageControllerTest.php
+++ laravel/tests/Feature/Mypage/BlogMypageControllerTest.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Mypage;
 
-use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 /**
  * @internal
  * @coversNothing
+ *
  * @see App\Http\Controllers\Mypage\BlogMypageController
  */
 final class BlogMypageControllerTest extends TestCase
@@ -22,8 +22,9 @@ final class BlogMypageControllerTest extends TestCase
         // $this->withoutExceptionHandling();
         // 認証している場合
         $this->get('mypage/blogs')
-            ->assertRedirect('mypage/login');
-        
+            ->assertRedirect('mypage/login')
+        ;
+
         // 認証済みの場合
         $this->login();
         $this->get('mypage/blogs')->assertOk();
