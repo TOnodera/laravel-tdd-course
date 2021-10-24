4eef2b386d35987456f7e897b99d6c32c53f63fc
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-10-24 19:28:03 +0900

    完了

--- laravel/resources/views/layouts/index.blade.php
+++ laravel/resources/views/layouts/index.blade.php
@@ -7,6 +7,22 @@
     <title>ブログ</title>
 </head>
 <body>
+
+    <nav>
+        <li><a href="/">TOP（ブログ一覧）</a></li>
+        @auth
+        <li><a href="/mypage/blogs">マイブログ一覧</a></li>
+        <li>
+            <form action="/mypage/logout" method="post">
+                @csrf
+                <input type="submit" value="ログアウト">
+            </form>
+        </li>
+        @else
+            <li><a href="{{ route('login') }}">ログイン</a></li>
+        @endauth
+    </nav>
+
     @yield('content')
 </body>
 </html>
