c59c005c9a4d084731432da21ac19c2593b46a18
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-10-25 11:38:10 +0900

    完了

---
+++ laravel/resources/views/mypage/blog/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.index')
+
+@section('content')
+
+<h1>マイブログ新規登録</h1>
+
+<a href='/mypage/blogs/create'>ブログ新規作成</a>
+<hr />
+
+<form action="/mapage/blogs" method="post">
+@include('inc.error')
+
+<div>タイトル: <input type="text" style="width: 400px" value="{{ old('title')}}" /></div>
+<div>本文: <textarea name="body" style="width: 600px;height: 200px;">{{ old('body') }}</textarea></div>
+<div>
+公開する: 
+<label>
+    <input type="checkbox" name="published" value="1" {{ old('published') ? 'checked': ''}}>
+    公開する
+</label>
+</div>
+<div>
+<input type="submit" value="送信する">
+</div>
+</form>
+
+
+
+@endsection
